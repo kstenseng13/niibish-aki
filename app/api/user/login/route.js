9f861b013c696da7f42059e9c2ecc9be2cfd526f
Kailee Stenseng <e-bird14@hotmail.com>
2025-04-02 12:19:22 -0500

    bad practices incoming :P

--- app/api/user/login/route.js
+++ app/api/user/login/route.js
@@ -15,7 +15,7 @@ export async function POST(req) {
         logger.info("POST request received for user login.");
 
         const { username, password } = await req.json();
-        logger.info("Username received.")
+        logger.info(uri);
         const client = new MongoClient(uri);
         await client.connect();
         logger.info("Connected to MongoDB.");
